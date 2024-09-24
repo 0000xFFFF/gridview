d6b6839ca4f287e016f52052e2114bd7407a6b50
0000xFFFF <coldmailgg@gmail.com>
2024-09-24 04:08:19 +0200

    added install scripts

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -x
+
+# Define the relative path to the icon
+relative_icon_path="assets/icon.png"
+
+# Get the absolute path to the icon
+full_icon_path="$(realpath "$relative_icon_path")"
+
+# Create the symbolic link to the application
+sudo ln -sfr release-builds/gridview-linux-x64/gridview /usr/local/bin/gridview
+
+# Generate the gridview.desktop file
+cat << EOF > /home/user/.local/share/applications/gridview.desktop
+[Desktop Entry]
+Version=1.0
+Name=GridView
+Exec=/usr/local/bin/gridview
+Icon=$full_icon_path
+Type=Application
+Terminal=false
+EOF
+
+# Update the desktop database (optional)
+update-desktop-database ~/.local/share/applications/
---
+++ uninstall.sh
@@ -0,0 +1,3 @@
+#!/bin/bash -x
+sudo rm /usr/local/bin/gridview
+sudo rm ~/.local/share/applications/gridview.desktop
